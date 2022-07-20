41586446e03a6ae196d7b770cc40e770938af359
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-07-20 12:25:56 -0600

    adds parent route and nested routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import RegistrationPage from "./Pages/RegistrationPage";
 import LoginPage from "./Pages/LoginPage";
-import HomePage from './Pages/HomePage'
+import HomePage from './Pages/HomePage';
+import Navbar from './Components/NavBar'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <RegistrationPage />
-        <LoginPage />
-        <HomePage/>
+        <Routes>
+          <Route path="/" element={<Navbar/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="login" element={<LoginPage/>}/>
+          <Route path="registration" element={<RegistrationPage/>}/>
+        </Route>
+        </Routes>
       </header>
     </div>
   );
